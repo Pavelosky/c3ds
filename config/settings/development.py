7f2b2aa05f2be3d88bdbb260af67fc184e69d83d
Pavelosky <pawel.bialkowskigru@gmail.com>
2025-12-03 20:58:18 +0100

    Add initial Django project structure with ASGI, WSGI, and settings configuration

---
+++ config/settings/development.py
@@ -0,0 +1,21 @@
+from .base import *
+from decouple import config
+
+# Debug should be True for development
+DEBUG = True
+
+# For development, we'll use SQLite
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Allow all hosts for development
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# Development-specific installed apps (we'll add debug toolbar later)
+INSTALLED_APPS += [
+    # We'll add development-specific apps here later
+]
