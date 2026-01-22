9e1b48698acdd92473454afbb18a4951312b5cc2
Pavelosky <pawel.bialkowskigru@gmail.com>
2026-01-22 20:09:16 +0100

    backend ready for participant dashboard + Swagger

--- apps/device_management/api_urls.py
+++ apps/device_management/api_urls.py
@@ -5,11 +5,13 @@ File: apps/device_managaement/api_urls.py
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.device_management.api_views import PublicDeviceViewSet
+from apps.device_management.api_views import PublicDeviceViewSet, ParticipantDeviceViewSet
+
 
 # Create router for ViewSets
 router = DefaultRouter()
 router.register('public', PublicDeviceViewSet, basename='device-public')
+router.register('participant', ParticipantDeviceViewSet, basename='device-participant')
 
 urlpatterns = [
     path('', include(router.urls)),
