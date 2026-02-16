47f8c2c8e17fa68e396f9e8e6beb98b090d1a357
Pavelosky <pawel.bialkowskigru@gmail.com>
2026-02-16 18:56:04 +0100

    more config for railway

--- config/settings/production.py
+++ config/settings/production.py
@@ -52,6 +52,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ALLOWED_ORIGINS = ["https://c3ds-frontend-production.up.railway.app",'http://localhost:5173',  # Vite dev server
     'http://127.0.0.1:5173',]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
 
